c1a65facdb6566ed2900ca43785f22977996e6cc
hyerin <dev.hr.kim@gamil.com>
2025-03-28 17:17:06 +0900

    first commit

---
+++ craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/CraftverseBlogApiApplication.java
@@ -0,0 +1,13 @@
+package kr.co.craftverse.craftverse_blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftverseBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftverseBlogApiApplication.class, args);
+	}
+
+}
---
+++ craftverse-blog-api/src/test/java/kr/co/craftverse/craftverse_blog_api/CraftverseBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.craftverse.craftverse_blog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftverseBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
