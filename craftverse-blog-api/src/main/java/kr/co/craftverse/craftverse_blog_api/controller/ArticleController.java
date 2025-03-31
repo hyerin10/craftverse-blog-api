edf32d6a75b12b6b9ba793cabd1feea8ec91afee
hyerin10 <dev.hr.kim@gmail.com>
2025-03-31 16:16:46 +0900

    refactor: API 데이터 반환 타입 변경

--- craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/controller/ArticleController.java
+++ craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/controller/ArticleController.java
@@ -6,6 +6,7 @@ import kr.co.craftverse.craftverse_blog_api.common.RestResult;
 import kr.co.craftverse.craftverse_blog_api.model.dto.ArticleDTO;
 import kr.co.craftverse.craftverse_blog_api.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,43 +17,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class ArticleController {
   private final ArticleService articleService;
 
   @GetMapping("/articles")
-  public RestResult getAll() {
+  public RestResult<Map<String, Object>> getAll() {
     Map<String, Object> data = new LinkedHashMap<>();
     data.put("articles", articleService.getAll());
-    return new RestResult(data);
+    return new RestResult<>(data);
   }
 
   @GetMapping("/article/{id}")
-  public RestResult getById(@PathVariable long id) {
+  public RestResult<Map<String, Object>> getById(@PathVariable long id) {
     Map<String, Object> data = new LinkedHashMap<>();
     data.put("article", articleService.getById(id));
-    return new RestResult(data);
+    return new RestResult<>(data);
   }
 
   @PostMapping("/article")
-  public RestResult create(@RequestBody ArticleDTO articleDTO) {
+  public RestResult<Map<String, Object>> create(@RequestBody ArticleDTO articleDTO) {
     Map<String, Object> data = new LinkedHashMap<>();
     data.put("article", articleService.create(articleDTO));
-    return new RestResult(data);
+    return new RestResult<>(data);
   }
 
   @PatchMapping("/article/{id}")
-  public RestResult update(@PathVariable("id") long id, @RequestBody ArticleDTO articleDTO)
+  public RestResult<Map<String, Object>> update(@PathVariable("id") long id, @RequestBody ArticleDTO articleDTO)
       throws Exception {
     Map<String, Object> data = new LinkedHashMap<>();
     data.put("article", articleService.update(id, articleDTO));
-    return new RestResult(data);
+    return new RestResult<>(data);
   }
 
   @DeleteMapping("/article/{id}")
-  public RestResult delete(@PathVariable("id") long id) throws Exception {
+  public RestResult<Map<String, Object>> delete(@PathVariable("id") long id) throws Exception {
     Map<String, Object> data = new LinkedHashMap<>();
     articleService.delete(id);
     data.put("success", "true");
-    return new RestResult(data);
+    return new RestResult<>(data);
   }
 }
