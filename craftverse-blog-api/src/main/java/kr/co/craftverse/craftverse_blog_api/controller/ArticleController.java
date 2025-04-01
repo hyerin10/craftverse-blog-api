8cb3bb87c7c7a4c17ddadf1f5efb9b202d716e14
hyerin <dev.hr.kim@gamil.com>
2025-04-01 22:10:38 +0900

    feature: CORS 설정 구현

--- craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/controller/ArticleController.java
+++ craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/controller/ArticleController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
 public class ArticleController {
   private final ArticleService articleService;
 
