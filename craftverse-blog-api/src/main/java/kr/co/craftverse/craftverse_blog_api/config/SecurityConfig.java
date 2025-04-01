c64a6dc5bf641b9ce070080bb879733e15f40e14
hyerin10 <dev.hr.kim@gmail.com>
2025-04-01 22:10:38 +0900

    feature: CORS 설정 구현

--- craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/config/SecurityConfig.java
+++ craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
             .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
 
         .authorizeHttpRequests(authorizeRequest -> authorizeRequest
-            .requestMatchers("/users/**", "/error").permitAll()
+            .requestMatchers("/users/**", "/articles", "/error").permitAll()
             .anyRequest().authenticated()
         )
 
