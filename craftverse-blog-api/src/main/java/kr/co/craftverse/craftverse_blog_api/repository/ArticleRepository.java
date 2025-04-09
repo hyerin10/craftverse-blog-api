c204211937cc5c45a98419f8012b93f493d40b1d
hyerin10 <dev.hr.kim@gmail.com>
2025-04-09 21:58:44 +0900

    feature: article, premium article 조회 시 language에 따라 분리 기능 추가

--- craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/repository/ArticleRepository.java
+++ craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/repository/ArticleRepository.java
@@ -1,10 +1,11 @@
 package kr.co.craftverse.craftverse_blog_api.repository;
 
+import java.util.List;
 import kr.co.craftverse.craftverse_blog_api.model.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+  List<Article> findByLanguage(String language);
 }
