b1a83857c001f64d90f2762864ccef8eba798b5b
hyerin10 <dev.hr.kim@gmail.com>
2025-06-04 18:46:24 +0900

    feature: AUth, Google Auth 관련 테스트 코드 작성

--- craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/model/dto/GoogleLoginRequestDTO.java
+++ craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/model/dto/GoogleLoginRequestDTO.java
@@ -1,12 +1,14 @@
 package kr.co.craftverse.craftverse_blog_api.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class GoogleLoginRequestDTO {
-  @NotBlank(message = "인증 코드는 필수입니다.")
   private String code;
-
+  private String state;
   private String redirectUri;
+  private String error;
 }
