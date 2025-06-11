258b236bb12d8dafba13313adf2770ef26e4b388
hyerin <dev.hr.kim@gamil.com>
2025-06-11 18:20:19 +0900

    fix: google 로그인 url 허용

--- craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/config/SecurityConfig.java
+++ craftverse-blog-api/src/main/java/kr/co/craftverse/craftverse_blog_api/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         .formLogin(AbstractHttpConfigurer::disable)
         .httpBasic(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/auth/register", "/auth/login", "/auth/logout", "/auth/verify-email", "/auth/google/url", "/auth/google/login", "/auth/refresh").permitAll()
+            .requestMatchers("/auth/register", "/auth/login", "/auth/logout", "/auth/verify-email", "/auth/google/url","/auth/google/callback", "/auth/google/callback/**", "/oauth2/**", "/auth/google/login", "/auth/refresh").permitAll()
             .requestMatchers("/article/*/views").permitAll()
             .requestMatchers("/article/**").permitAll()
             .requestMatchers("/articles").permitAll()
